1e7eab32b3e41f46a33ed9d09e1e27b43f60ee77
DarylPJ <34217093+DarylPJ@users.noreply.github.com>
2023-08-04 20:39:14 +0100

    🚧 Add UI for filtering

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          {children}
+        </LocalizationProvider>
+      </body>
     </html>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,12 @@
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import { NewsCard } from "@/components/news-card";
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import { FormControl, IconButton, InputLabel, MenuItem } from "@mui/material";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ClearIcon from "@mui/icons-material/Clear";
 
 interface ISource {
   Id: string | null;
@@ -19,6 +25,26 @@ interface IArticle {
   description: string;
 }
 
+function renderArticles(articles: IArticle[]) {
+  return (
+    <div className={styles["grid-container"]}>
+      {articles.map((article, index) => (
+        <div key={`item-${index}`} className={styles["grid-item"]}>
+          <NewsCard
+            key={`news-{index}`}
+            title={article.title}
+            sourceName={article.source?.Name ?? ""}
+            description={article.description}
+            image={article.urlToImage}
+            url={article.url}
+            published={article.publishedAt}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export default function Home() {
   const [data, setData] = useState<IArticle[]>([]);
 
@@ -33,31 +59,58 @@ export default function Home() {
     setData(text);
   }
 
+  function renderHeader() {
+    return (
+      <div className={styles["header"]}>
+        <div className={styles["flex-container"]}>
+          <div className={styles["flex-item"]}>
+            <TextField
+              id="outlined-basic"
+              label="Search"
+              variant="outlined"
+              fullWidth
+              size="small"
+            />
+          </div>
+          <IconButton size="small">
+            <ClearIcon />
+          </IconButton>
+        </div>
+        <div className={styles["flex-container"]}>
+          <DateTimePicker
+            label="From"
+            slotProps={{ textField: { size: "small" } }}
+          />
+          <DateTimePicker
+            label="To"
+            slotProps={{ textField: { size: "small" } }}
+          />
+          <div className={styles["flex-item"]}>
+            <FormControl fullWidth size="small">
+              <InputLabel>Sort By</InputLabel>
+              <Select value={"pubishedAt"} label="Sort By">
+                <MenuItem value="title">Title</MenuItem>
+                <MenuItem value="author">Author</MenuItem>
+                <MenuItem value="pubishedAt">PubishedAt</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <IconButton size="small">
+            <ArrowUpwardIcon />
+          </IconButton>
+        </div>
+      </div>
+    );
+  }
+
   if (!data.length) {
     return <main>Loading...!</main>;
   }
 
-  return <main className={styles["main"]}>{renderArticles(data)}</main>;
-}
-
-function renderArticles(articles: IArticle[]) {
   return (
-    <div className={styles["page-container"]}>
-      <div className={styles["grid-container"]}>
-        {articles.map((article, index) => (
-          <div key={`item-${index}`} className={styles["grid-item"]}>
-            <NewsCard
-              key={`news-{index}`}
-              title={article.title}
-              sourceName={article.source?.Name ?? ""}
-              description={article.description}
-              image={article.urlToImage}
-              url={article.url}
-              published={article.publishedAt}
-            />
-          </div>
-        ))}
-      </div>
-    </div>
+    <main className={styles["main"]}>
+      {renderHeader()}
+      {renderArticles(data)}
+    </main>
   );
 }
